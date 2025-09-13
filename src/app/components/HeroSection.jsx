6fdf69124892023eb60467b69e81927b8b03f800
Shrishant Pandey <138847244+Shrishant2325@users.noreply.github.com>
2025-09-14 01:41:55 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
               sequence={[
                 "Shrishant",
                 1000,
-                "Web Developer",
+                "Full Stack Developer",
                 1000,
                 "Java Programmer",
                 1000,
@@ -49,7 +49,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1RDT5v55EJy0tdEB0CBrP4G94gJeyA8h4/view?usp=sharing"
+              href="https://drive.google.com/drive/folders/1_96qT5cFHs30CdjQ21SgykNKdkM1LPQg?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -80,4 +80,5 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
 
